d420d6aa385fe174e6b24dbd5e2dee09afa32b16
kubekthecreator <50717269+kubekthecreator@users.noreply.github.com>
2024-02-21 16:53:54 +0100

    add video metadata form

---
+++ backend/src/main/java/com/kubekthecreator/projects/youtubeclone/dto/VideoDto.java
@@ -0,0 +1,22 @@
+package com.kubekthecreator.projects.youtubeclone.dto;
+
+
+import com.kubekthecreator.projects.youtubeclone.model.VideoStatus;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VideoDto {
+    private String id;
+    private String title;
+    private String description;
+    private Set<String> tags;
+    private String videoUrl;
+    private VideoStatus videoStatus;
+    private String thumbnailUrl;
+}
--- backend/src/main/java/com/kubekthecreator/projects/youtubeclone/service/VideoService.java
+++ backend/src/main/java/com/kubekthecreator/projects/youtubeclone/service/VideoService.java
@@ -1,5 +1,7 @@
 package com.kubekthecreator.projects.youtubeclone.service;
 
+import com.kubekthecreator.projects.youtubeclone.dto.UploadVideoResponse;
+import com.kubekthecreator.projects.youtubeclone.dto.VideoDto;
 import com.kubekthecreator.projects.youtubeclone.model.Video;
 import com.kubekthecreator.projects.youtubeclone.repository.VideoRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,10 +15,35 @@ public class VideoService {
     private final S3Service s3Service;
     private final VideoRepository videoRepository;
 
-    public void uploadVideo(MultipartFile file) {
+    public UploadVideoResponse uploadVideo(MultipartFile file) {
         String videoUrl = s3Service.uploadFile(file);
         var video = new Video();
         video.setVideoUrl(videoUrl);
-        videoRepository.save(video);
+        var savedVideo = videoRepository.save(video);
+        return new UploadVideoResponse(savedVideo.getId(),savedVideo.getVideoUrl());
+    }
+
+    public VideoDto editVideo(VideoDto videoDto) {
+        var savedVideo = getVideById(videoDto.getId());
+        savedVideo.setTitle(videoDto.getTitle());
+        savedVideo.setDescription(videoDto.getDescription());
+        savedVideo.setTags(videoDto.getTags());
+        savedVideo.setThumbnailUrl(videoDto.getThumbnailUrl());
+        savedVideo.setVideoStatus(videoDto.getVideoStatus());
+        videoRepository.save(savedVideo);
+        return videoDto;
+    }
+
+    public String uploadThumbnail(MultipartFile file, String videoId) {
+        var foundVideo = getVideById(videoId);
+        String thumbnailUrl = s3Service.uploadFile(file);
+        foundVideo.setThumbnailUrl(thumbnailUrl);
+        videoRepository.save(foundVideo);
+        return thumbnailUrl;
+    }
+
+    private Video getVideById(String videoId) {
+        return videoRepository.findById(videoId)
+                .orElseThrow(() -> new IllegalArgumentException("Cannot find video by id " + videoId));
     }
 }
